c34211b1d1bde87c0e593250984b371b303ad221
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-10 14:31:14 +0100

    ok

---
+++ app/controllers/journeys_controller.rb
@@ -0,0 +1,20 @@
+class JourneysController < ApplicationController
+  def create
+    @pigeon = Pigeon.find(params[:pigeon_id])
+    @journey = Journey.new(params_journey)
+    @journey.user = current_user
+    @journey.pigeon = @pigeon
+      if @journey.save
+     redirect_to dashboard_path(@pigeon)
+      else
+      render "pigeon_path" #page pigeon_id
+      end
+  end
+
+  private
+
+  def params_journey
+    params.require(:journey).permit(:user_id, :pigeon_id, :arrival_address,
+      :total_price, :arrival_date, :message)
+  end
+end
