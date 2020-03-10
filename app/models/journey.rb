c000f0d3ca58a76760d34ad1977cd46e765bbc95
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-10 16:33:52 +0100

    ajouté validations + dependances + has_many dans pigeon

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,4 +1,7 @@
 class Journey < ApplicationRecord
   belongs_to :user
   belongs_to :pigeon
+  validates :arrival_address, presence: true
+  validates :message, presence: true
+  validates :arrival_date, presence: true
 end
