35acb875bfee32b1e36b51acd5d5532f2ab0738a
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-10 11:36:17 +0100

    adding name and address to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_03_10_095634) do
+ActiveRecord::Schema.define(version: 2020_03_10_103522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "journeys", force: :cascade do |t|
     t.bigint "user_id"
@@ -49,10 +47,13 @@ ActiveRecord::Schema.define(version: 2020_03_10_095634) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "journeys", "pigeons"
+  add_foreign_key "journeys", "users"
   add_foreign_key "pigeons", "users"
 end
