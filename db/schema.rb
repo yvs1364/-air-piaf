abb61ceb5d95a3626863ca0c2b52dbe1abcd7a26
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-10 10:57:18 +0100

    journey model and journeys table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_092119) do
+ActiveRecord::Schema.define(version: 2020_03_10_095634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "journeys", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "pigeon_id"
+    t.string "arrival_address"
+    t.text "message"
+    t.integer "total_price"
+    t.date "arrival_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pigeon_id"], name: "index_journeys_on_pigeon_id"
+    t.index ["user_id"], name: "index_journeys_on_user_id"
+  end
+
+  create_table "pigeons", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.integer "km_per_hour"
+    t.integer "price_per_hour"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pigeons_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +51,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_092119) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "journeys", "pigeons"
+  add_foreign_key "journeys", "users"
+  add_foreign_key "pigeons", "users"
 end
