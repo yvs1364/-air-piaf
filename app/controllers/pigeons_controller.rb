1e6c9f09035e162a0eaeb773634bdf453b72e203
yvan poussard <yvs.pro64@gmail.com>
2020-03-10 14:07:36 +0100

    controllers avec quelques views

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -8,16 +8,24 @@ class PigeonsController < ApplicationController
   end
 
   def show
+    @user = current_user
     @pigeon = Pigeon.find(params)
+    @journey = Journey.new
   end
 
   def create
+    @pigeon = Pigeon.new(pigeon_params)
+    if @pigeon.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   private
 
   def pigeon_params
-    params.require(:pigeon).permit(:name, :breed, :km_per_hours, :price_per_hour,:users_id)
+    params.require(:pigeon).permit(:name, :breed, :km_per_hours, :price_per_hour,:users_id) #rajouter photo
 
   end
 end
