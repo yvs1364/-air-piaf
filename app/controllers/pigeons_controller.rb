913bf0b6799f1c3c6f644cc60aacbb37f1bfc4d3
yvan poussard <yvs.pro64@gmail.com>
2020-03-10 11:27:12 +0100

    model schema controller-pigeons

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,13 +1,23 @@
 class PigeonsController < ApplicationController
   def index
+    @pigeons = pigeon.all
   end
 
   def new
+    @pigeon = Pigeon.new
   end
 
   def show
+    @pigeon = Restaurant.find(params[:id])
   end
 
   def create
+  end
+
+  private
+
+  def pigeon_params
+    params.require(:pigeon).permit(:breed)
+
   end
 end
