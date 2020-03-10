692bf37e6f912fd12dce2525a0a731164db00a97
yvan poussard <yvs.pro64@gmail.com>
2020-03-10 14:48:33 +0100

    migrate ok

--- db/migrate/20200310132312_rename_price_per_hour_to_price_per_km.rb
+++ db/migrate/20200310132312_rename_price_per_hour_to_price_per_km.rb
@@ -1,12 +1,6 @@
 class RenamePricePerHourToPricePerKm < ActiveRecord::Migration[5.2]
   def change
-<<<<<<< HEAD
-    rename_column :price_per_hour, :price_per_km
-  end
-end
-
-=======
     rename_column :pigeons, :price_per_hour, :price_per_km
   end
 end
->>>>>>> b47fcff082a57ef5f26ee03722299beb081db122
+
