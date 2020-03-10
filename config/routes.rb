877ff8f2da05803cbab5a3f9f38d8c1d2388404d
Yvan Curbillon <yvancurbillon888@gmail.com>
2020-03-10 11:12:48 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources: :pigeons, :only w%(index new show create)do
-  resouces: :journeys, only: :create
+  resources :pigeons, :only w%(index new show create) do
+    resouces :journeys, only: :create
+  end
 end
