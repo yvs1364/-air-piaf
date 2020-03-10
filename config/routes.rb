d79900345b2d72a8becae41473cbb1683491862e
yvan poussard <yvs.pro64@gmail.com>
2020-03-10 10:24:51 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources: :pigeons, :only w%(index new show create)do
+  resouces: :journeys, only: :create
 end
