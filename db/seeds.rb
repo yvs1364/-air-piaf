20acbf298eaab1f275a995274ff26975992a8177
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-10 16:22:13 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ puts "#{User.count} users created"
     name: Faker::Name.middle_name,
     breed: Faker::Hacker.adjective,
     km_per_hour: rand(1..10),
-    price_per_hour: rand(1..10),
+    price_per_km: rand(1..10),
     user_id: rand(1..3)
   )
 end
