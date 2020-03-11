2996948712230934465bdb90df9cba9bbe84878e
Yvan Curbillon <yvancurbillon888@gmail.com>
2020-03-11 16:36:58 +0100

    design home + seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
