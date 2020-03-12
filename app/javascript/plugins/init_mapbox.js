88a78aee5a5d0b26695f08fd34f43e1d2ee5a6e1
yvan poussard <yvs.pro64@gmail.com>
2020-03-12 17:30:32 +0100

    mappppppppp oooook

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,37 +10,46 @@ const addMarkersToMap = (map, markers) => {
     element.style.backgroundSize = 'contain';
     element.style.width = '40px';
     element.style.height = '40px';
-    // element.style.borderRadius = '50px'
-
 
     new mapboxgl.Marker(element)
-      .setLngLat([ marker.lng, marker.lat ])
+    .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
       .addTo(map);
-  });
+    });
 };
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 20, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 30, duration: 0 });
 };
 
-
 const initMapbox = () => {
+  const cards = document.querySelectorAll(".card-category")
   const mapElement = document.getElementById('map');
-
-  if (mapElement) { // only build a map if there's a div#map to inject into
+  if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
-        });
-
+    });
     const markers = JSON.parse(mapElement.dataset.markers);
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
+    cards.forEach((card) => {
+      card.addEventListener("click", (event) => {
 
-     addMarkersToMap(map, markers);
-     fitMapToMarkers(map, markers);
+         mapElement.classList.add("visible");
+        const lng = parseFloat(card.dataset.lng, 10);
+        const lat = parseFloat(card.dataset.lat, 10);
+        map.flyTo({
+          center: [lng, lat],
+          zoom: 9,
+          speed: 1,
+        });
+      });
+    });
   };
 };
+
 export { initMapbox };
