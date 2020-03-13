d9edd186d1128b219909607d0e7fe4cd64c32455
Mehdi Mosbah <mosbah.meh.didi@gmail.com>
2020-03-13 10:49:27 +0100

    init pendit

---
+++ app/policies/pigeon_policy.rb
@@ -0,0 +1,18 @@
+class PigeonPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def create?
+      return true
+    end
+
+    def update?
+      record.user == user
+    end
+
+    def destroy?
+    record.user == user
+  end
+end
