9bac52e3eb004d9f7536279a8cde2447762eefb3
yvan poussard <yvs.pro64@gmail.com>
2020-03-14 08:19:44 +0100

    pblm ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 Pigeon.destroy_all
 User.destroy_all
 mehdi = User.create!(name: 'Mehdi', email: 'mehdi@mail.fr', password: 'password', address: 'Paris')
@@ -15,17 +16,16 @@ manu = User.create!(name: 'Manu', email: 'manu@mail.fr', password: 'password', a
 
 puts "#{User.count} users created"
 
-
-pigeon_names = %w[MattiOL Celio thibthibthib Laulau Beber Thethe Raphou eveuuuuuhh Seb Patoche Estelleuuuhh Jeromeeeeeuuuh Loulou Alixbaba Cams Gâteau Kevdu13 Deivid]
-pigeon_names.each do |pigeon_name|
-  Pigeon.create!(
-    name: pigeon_name,
+18.times do
+ pigeon = Pigeon.create!(
+    name: %w[MattiOL Celio thibthibthib Laulau Beber Thethe Raphou eveuuuuuhh Seb Patoche Estelleuuuhh Jeromeeeeeuuuh Loulou Alixbaba Cams Gâteau Kevdu13 Deivid].sample,
     breed: ['Ice Pigeon', 'Antwerp Smerle', 'Oriental Roller', 'Egyptian Swift pigeon', 'Homing pigeon', 'Lahore pigeon'].sample,
     km_per_hour: rand(50..120),
     price_per_km: rand(10..30),
     address: %w[Marseille Paris Lyon Pau Genay Bamako Bordeaux cleveland berlin lisbonne milan tokyo].sample,
-    user: [yvan, manu, mehdi].sample
+    user: [yvan, manu, mehdi].sample,
   )
+    file = URI.open('https://source.unsplash.com/featured/?birds')
+    pigeon.photo.attach(io:file,filename: 'pigeon.jpeg')
 end
 puts "#{Pigeon.count} pigeons created"
-
